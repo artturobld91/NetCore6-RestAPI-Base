f56e6433688cd7d2f762b4363351211dc6278434
artturobld <artturobld@gmail.com>
2021-12-13 22:11:22 -0600

    Added Swagger configuration

--- RestAPINet6Base/RestAPINet6Base/Startup.cs
+++ RestAPINet6Base/RestAPINet6Base/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 
 namespace RestAPINet6Base
 {
@@ -42,6 +43,8 @@ namespace RestAPINet6Base
             );
 
             services.AddControllers();
+
+            AddSwagger(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +57,12 @@ namespace RestAPINet6Base
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "REST Base API V1");
+            });
+
             app.UseRouting();
 
             app.UseCors();
@@ -63,6 +72,51 @@ namespace RestAPINet6Base
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                var groupName = "v1";
+
+                options.SwaggerDoc(groupName, new OpenApiInfo
+                {
+                    Title = $"REST Base {groupName}",
+                    Version = groupName,
+                    Description = "REST Base API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "ArtturoBld",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/ArtturoBld"),
+                    }
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[]{ }
+                    }
+                });
             });
         }
     }
